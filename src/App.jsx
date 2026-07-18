a7e26a4e712226b700a739196713cdc9037827d8
dhruv <dhruvmehalawat34@gmail.com>
2026-07-18 16:29:25 +0530

    features section complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
-const App = () => {
+import Features from "./components/Features";
 
+const App = () => {
   return (
     <main className="bg-black">
-    <Nav/>
-    <Hero/>
-    <Highlights/>
-    <Model/>
+      <Nav />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features/>
     </main>
-  )
-}
+  );
+};
 
-export default App
+
+export default App;
